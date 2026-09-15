694ed15e3a5ba703050d13a42ccc63a31c5c91b4
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-16 00:55:41 +0200

    fix: reject open area fragments

---
+++ release-18/assets/__vite-browser-external-BFkSHh1f.js
@@ -0,0 +1,2 @@
+import{t as e}from"./index-DsvUC6LC.js";var t=e(((e,t)=>{t.exports={}}));export default t();
+//# sourceMappingURL=__vite-browser-external-BFkSHh1f.js.map
--- release-18/service-worker.js
+++ release-18/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="skaner-studio-offline-release-46-smoothperf";
+const CACHE="skaner-studio-offline-release-47-smoothperf";
 const SHELL=["./?offline=1","./manifest.webmanifest","./assets/skaner-icon.png"];
 self.addEventListener("install",event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE),response=await fetch("./"),html=await response.text();
