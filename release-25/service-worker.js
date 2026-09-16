0bb8e4a09f01b3c4b5d68e752bd57be689e8b1bc
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-17 01:51:24 +0200

    stabilize mobile touch rendering

--- release-25/service-worker.js
+++ release-25/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="skaner-studio-offline-release-91-nomad-local-brush";
+const CACHE="skaner-studio-offline-release-92-mobile-touch-stable";
 const SHELL=["./?offline=1","./manifest.webmanifest","./assets/skaner-icon.png"];
 self.addEventListener("install",event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE),response=await fetch("./"),html=await response.text();
