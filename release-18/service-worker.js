16a8a7209c307768bd21d19f263752948425343d
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-16 18:17:19 +0200

    Refine sculpt density for responsive mobile strokes

--- release-18/service-worker.js
+++ release-18/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="skaner-studio-offline-release-59-taubin-sculpt";
+const CACHE="skaner-studio-offline-release-60-sculpt-refinement";
 const SHELL=["./?offline=1","./manifest.webmanifest","./assets/skaner-icon.png"];
 self.addEventListener("install",event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE),response=await fetch("./"),html=await response.text();
