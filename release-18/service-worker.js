594a6d6dc1470b14dd5037873d08f20c87e1b680
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-16 18:27:25 +0200

    Synchronize visible build marker

--- release-18/service-worker.js
+++ release-18/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="skaner-studio-offline-release-62-b55-footer";
+const CACHE="skaner-studio-offline-release-63-b55-marker";
 const SHELL=["./?offline=1","./manifest.webmanifest","./assets/skaner-icon.png"];
 self.addEventListener("install",event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE),response=await fetch("./"),html=await response.text();
