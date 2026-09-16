4c7175c794b7bf8ca1e328448290321ba0ce970e
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-16 19:15:11 +0200

    Add stable elastic sculpt refinement

--- release-18/service-worker.js
+++ release-18/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="skaner-studio-offline-release-67-local-sculpt";
+const CACHE="skaner-studio-offline-release-55-progressive-worker";
 const SHELL=["./?offline=1","./manifest.webmanifest","./assets/skaner-icon.png"];
 self.addEventListener("install",event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE),response=await fetch("./"),html=await response.text();
