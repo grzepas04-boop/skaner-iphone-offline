f9dedb81df66175c9123ce1fffe9ecd34d849099
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-16 16:07:05 +0200

    Add progressive mobile refinement

---
+++ release-18/assets/__vite-browser-external-BwL3PD4f.js
@@ -0,0 +1,2 @@
+import{t as e}from"./index-Ep5e8BNw.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
+//# sourceMappingURL=__vite-browser-external-BwL3PD4f.js.map
--- release-18/assets/__vite-browser-external-DsoQ5Bxk.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./index-Dl9Gkbta.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
-//# sourceMappingURL=__vite-browser-external-DsoQ5Bxk.js.map
