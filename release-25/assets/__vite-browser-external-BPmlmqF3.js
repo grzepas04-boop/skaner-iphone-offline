1d73e42252fd09a5f242ecef16a9e83124f10fb4
grzepas04-boop <grzepas04-boop@users.noreply.github.com>
2026-09-17 20:03:29 +0200

    add multiresolution sculpting support

---
+++ release-25/assets/__vite-browser-external-BPmlmqF3.js
@@ -0,0 +1,2 @@
+import{t as e}from"./index-BdzpsUxf.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
+//# sourceMappingURL=__vite-browser-external-BPmlmqF3.js.map
--- release-25/assets/__vite-browser-external-D7IDCwJa.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./index-B_LzVYdf.js";var t=e(((e,t)=>{t.exports={}}));export default t();export{t};
-//# sourceMappingURL=__vite-browser-external-D7IDCwJa.js.map
